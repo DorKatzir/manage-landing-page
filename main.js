e0db48dc5087d17829878834a205304e9a442b04
Dor Katzir <katzirdor@gmail.com>
2024-03-15 22:13:52 +0200

    Carousel Section setting up the A11YSlider js library

--- main.js
+++ main.js
@@ -11,3 +11,8 @@ navToggle.addEventListener('click', ()=>{
     primaryHeader.toggleAttribute('data-overlay')
 })
 
+ const slider = new A11YSlider(document.querySelector('.slider'), {
+		adaptiveHeight: true,
+		dots: false,
+ })
+
